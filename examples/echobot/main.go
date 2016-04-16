1058b9ecc6754a96c4e08453d1550c6e056e58ac
Andrey <and@rey.im>
2016-04-16 12:17:47 +0300

    Add example simple echo bot.

---
+++ examples/echobot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/onrik/micha"
+	"log"
+)
+
+func main() {
+	bot, err := micha.NewBot("<token>")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go bot.Start()
+
+	for update := range bot.Updates {
+		if update.Message != nil {
+			bot.SendMessage(update.Message.Chat.Id, update.Message.Text, nil)
+		}
+	}
+}
