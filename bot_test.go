99004a720b35b68d033a09a4b35c29e8a52f3a40
Andrey <and@rey.im>
2016-08-07 13:16:44 +0300

    Add tests.

--- bot_test.go
+++ bot_test.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -29,7 +32,11 @@ func (s *BotTestSuite) SetupSuite() {
 }
 
 func (s *BotTestSuite) TearDownSuite() {
-	httpmock.DeactivateAndReset()
+	httpmock.Deactivate()
+}
+
+func (s *BotTestSuite) TearDownTest() {
+	httpmock.Reset()
 }
 
 func (s *BotTestSuite) registerResponse(method string, params url.Values, response string) {
@@ -37,7 +44,23 @@ func (s *BotTestSuite) registerResponse(method string, params url.Values, respon
 	if params != nil {
 		url += fmt.Sprintf("?%s", params.Encode())
 	}
+
 	httpmock.RegisterResponder("GET", url, httpmock.NewStringResponder(200, response))
+}
+
+func (s *BotTestSuite) registerRequestCheck(method string, exceptedRequest string) {
+	url := s.bot.buildUrl(method)
+
+	httpmock.RegisterResponder("POST", url, func(request *http.Request) (*http.Response, error) {
+		defer request.Body.Close()
+		body, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		s.Equal(exceptedRequest, strings.TrimSpace(string(body)))
+		return httpmock.NewStringResponse(200, `{"ok":true, "result": {}}`), nil
+	})
 }
 
 func (s *BotTestSuite) TestBuildUrl() {
@@ -149,6 +172,148 @@ func (s *BotTestSuite) TestGetFile() {
 	s.Equal(file.FileId, "222")
 	s.Equal(file.FileSize, uint64(5))
 	s.Equal(file.FilePath, "document/file_3.txt")
+}
+
+func (s *BotTestSuite) TestDownloadFileUrl() {
+	url := s.bot.DownloadFileUrl("file.mp3")
+	s.Equal(url, "https://api.telegram.org/file/bot111/file.mp3")
+}
+
+func (s *BotTestSuite) TestSendPhoto() {
+	request := `{"chat_id":111,"photo":"35f9f497a879436fbb6e682f6dd75986","caption":"test caption","reply_to_message_id":143}`
+	s.registerRequestCheck("sendPhoto", request)
+
+	message, err := s.bot.SendPhoto(111, "35f9f497a879436fbb6e682f6dd75986", &SendPhotoOptions{
+		Caption:          "test caption",
+		ReplyToMessageId: 143,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendAudio() {
+	request := `{"chat_id":123,"audio":"061c2810391f44f6beffa3ee8a7e5af4","duration":36,"performer":"John Doe","title":"Single","reply_to_message_id":143}`
+	s.registerRequestCheck("sendAudio", request)
+
+	message, err := s.bot.SendAudio(123, "061c2810391f44f6beffa3ee8a7e5af4", &SendAudioOptions{
+		Duration:         36,
+		Performer:        "John Doe",
+		Title:            "Single",
+		ReplyToMessageId: 143,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendDocument() {
+	request := `{"chat_id":124,"document":"efd8d08958894a6781873b9830634483","caption":"document caption","reply_to_message_id":144}`
+	s.registerRequestCheck("sendDocument", request)
+
+	message, err := s.bot.SendDocument(124, "efd8d08958894a6781873b9830634483", &SendDocumentOptions{
+		Caption:          "document caption",
+		ReplyToMessageId: 144,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendSticker() {
+	request := `{"chat_id":125,"sticker":"070114a7fa964322acb3d65e6e36eb2b","reply_to_message_id":145}`
+	s.registerRequestCheck("sendSticker", request)
+
+	message, err := s.bot.SendSticker(125, "070114a7fa964322acb3d65e6e36eb2b", &SendStickerOptions{
+		ReplyToMessageId: 145,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendVideo() {
+	request := `{"chat_id":126,"video":"b169f647c020405b8c9035cf3f315ff0","duration":22,"width":320,"height":240,"caption":"video caption","reply_to_message_id":146}`
+	s.registerRequestCheck("sendVideo", request)
+
+	message, err := s.bot.SendVideo(126, "b169f647c020405b8c9035cf3f315ff0", &SendVideoOptions{
+		Duration:         22,
+		Width:            320,
+		Height:           240,
+		Caption:          "video caption",
+		ReplyToMessageId: 146,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendVoice() {
+	request := `{"chat_id":127,"voice":"75ac50947bc34a3ea2efdca5000d9ad5","duration":56,"reply_to_message_id":147}`
+	s.registerRequestCheck("sendVoice", request)
+
+	message, err := s.bot.SendVoice(127, "75ac50947bc34a3ea2efdca5000d9ad5", &SendVoiceOptions{
+		Duration:         56,
+		ReplyToMessageId: 147,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendLocation() {
+	request := `{"chat_id":128,"latitude":22.532434,"longitude":-44.8243324,"reply_to_message_id":148}`
+	s.registerRequestCheck("sendLocation", request)
+
+	message, err := s.bot.SendLocation(128, 22.532434, -44.8243324, &SendLocationOptions{
+		ReplyToMessageId: 148,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendVenue() {
+	request := `{"chat_id":129,"latitude":22.532434,"longitude":-44.8243324,"title":"Kremlin","address":"Red Square 1","foursquare_id":"1","reply_to_message_id":149}`
+	s.registerRequestCheck("sendVenue", request)
+
+	message, err := s.bot.SendVenue(129, 22.532434, -44.8243324, "Kremlin", "Red Square 1", &SendVenueOptions{
+		FoursquareId:     "1",
+		ReplyToMessageId: 149,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendContact() {
+	request := `{"chat_id":130,"phone_number":"+79998887766","first_name":"John","last_name":"Doe","reply_to_message_id":150}`
+	s.registerRequestCheck("sendContact", request)
+
+	message, err := s.bot.SendContact(130, "+79998887766", "John", "Doe", &SendContactOptions{
+		ReplyToMessageId: 150,
+	})
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestForwardMessage() {
+	request := `{"chat_id":131,"disable_notification":true,"from_chat_id":99,"message_id":543}`
+	s.registerRequestCheck("forwardMessage", request)
+
+	message, err := s.bot.ForwardMessage(131, 99, 543, true)
+
+	s.Equal(err, nil)
+	s.NotEqual(message, nil)
+}
+
+func (s *BotTestSuite) TestSendChatAction() {
+	request := `{"action":"typing","chat_id":132}`
+	s.registerRequestCheck("sendChatAction", request)
+
+	err := s.bot.SendChatAction(132, CHAT_ACTION_TYPING)
+	s.Equal(err, nil)
 }
 
 func TestBotTestSuite(t *testing.T) {
